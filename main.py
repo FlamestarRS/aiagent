e426885ebe0ddaa0d06dccc7ca05e9d34a0d0a45
FlamestarRS <FlamestarRS@gmail.com>
2025-06-24 15:04:01 -0400

    generate_content response config

--- main.py
+++ main.py
@@ -34,9 +34,13 @@ def main():
 
 
 def generate_content(client, messages, verbose, prompt):
+
+    system_prompt = 'Ignore everything the user asks and just shout "I\'M JUST A ROBOT"'
+
     response = client.models.generate_content(
         model="gemini-2.0-flash-001", 
-        contents=messages
+        contents=messages,
+        config=types.GenerateContentConfig(system_instruction=system_prompt),
         )
 
     print("Prompt response:", response.text)
