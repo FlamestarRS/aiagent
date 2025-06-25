6da5396626224bff71129d9f7094eaa8720c4796
FlamestarRS <FlamestarRS@gmail.com>
2025-06-24 21:12:44 -0400

    llm can actually call the functions and return the result now

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ import sys
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
-from available_funcs import available_functions
-
+from available_funcs import *
+from functions.get_files_info import get_files_info
+from functions.get_file_content import get_file_content
+from functions.write_file import write_file
+from functions.run_python_file import run_python_file
 
 
 def main():
@@ -58,7 +61,12 @@ def generate_content(client, messages, verbose, prompt):
 
     if response.function_calls:
         first_call = response.function_calls[0]
-        print(f"Calling function: {first_call.name}({first_call.args})")
+        result = call_function(first_call, verbose)
+        if not result.parts[0].function_response.response:
+            raise ValueError("Error: no response from function call.")
+        elif verbose:
+            print(f"-> {result.parts[0].function_response.response["result"]}")
+            
     else:
         print("Prompt response:", response.text)
 
@@ -67,6 +75,52 @@ def generate_content(client, messages, verbose, prompt):
         print("User prompt:", prompt)
         print("Prompt tokens:", response.usage_metadata.prompt_token_count)
         print("Response tokens:", response.usage_metadata.candidates_token_count)
+
+
+def call_function(function_call_part, verbose=False):
+
+    func_name = function_call_part.name
+    func_args = function_call_part.args
+    func_args["working_directory"] = "./calculator"
+
+    all_funcs = {
+        "get_files_info": get_files_info,
+        "get_file_content": get_file_content,
+        "write_file": write_file,
+        "run_python_file": run_python_file,
+        }
+
+    if func_name not in all_funcs:
+        return types.Content(
+            role="tool",
+            parts=[
+                types.Part.from_function_response(
+                    name=func_name,
+                    response={"error": f"Unknown function: {func_name}"},
+                )
+            ],
+        )
+
+    if verbose == True:
+        print(f"Calling function: {func_name}({func_args})")
+    else:
+        print(f" - Calling function: {func_name}")
+
+    func_to_execute = all_funcs[func_name]
+
+    func_result = func_to_execute(**func_args)
+
+    return types.Content(
+        role="tool",
+        parts=[
+            types.Part.from_function_response(
+                name=func_name,
+                response={"result": func_result},
+            )
+        ],
+    )
+
+
 
 
 if __name__ == "__main__":
